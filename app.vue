470630731d629a23965b74c5bcec6af35e73453b
Matt Schleifer <matthewschleifer@gmail.com>
2023-12-16 22:31:16 -0600

    add author signature

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="bg-surface-900 px-4 py-4 min-h-screen h-full">
     <NuxtPage />
+    <div class="text-center mt-4">
+      <a href="https://github.com/mschleifer/sleeper-salt-tracker">
+        <i class="pi pi-github"></i>
+      </a>
+      mschleifer
+    </div>
   </div>
 </template>
